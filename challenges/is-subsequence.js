27f1ce18a3687747d806e1c8f3422b46952feb61
Yaf98 <manuelyg2097@gmail.com>
2021-03-31 22:36:39 -0600

    Add 'is-subsequence' challenge

---
+++ challenges/is-subsequence.js
@@ -0,0 +1,44 @@
+/**
+ * Write a function called isSubsequence which takes in two strings and checks
+ * whether the characters in the first string form a subsequence of characters
+ * in the second string. In other words, the function should check whether the
+ * characters in the first string appear somewhere in the second string, without
+ * their order changing
+ */
+
+// Multiple pointers approach
+
+// Time Complexity O(n)
+// Space Complexity O(1)
+
+// Input: s1: String, s2: String
+  // start1 <- 0
+  // start2 <- 0
+  // while start2 < s2.length
+    
+    // if s2[start2] == s1[start1]
+      // start1++
+    // end if
+
+    // if start1 == s1.length
+      // return true
+    // end if
+    
+    // start2++
+  
+    // end while
+
+    // return false
+
+function isSubsequence (s1, s2) {
+  let start1 = 0
+  let start2 = 0
+
+  if(!s1) return true
+  while (start2 < s2.length) {
+    if (s2[start2] === s1[start1]) start1++
+    if (start1 === s1.length) return true
+    start2++
+  }
+  return false
+}
