ee72b7336653cdbd9d9b8c31bdb353398c65d7b5
manu-yaff <manuelyg2097@gmail.com>
2022-04-01 14:54:14 -0600

    remove old folder

--- old/recursion/factorial.js
+++
@@ -1,8 +0,0 @@
-/**
- * Function that calculates the factorial of a given number using recursion
- */
-
-function factorial (num) {
-  if (num === 1) return 1
-  return num * factorial(num - 1)
-}
--- old/recursion/fibonnaci.js
+++
@@ -1,9 +0,0 @@
-/**
- * Write a function call fib which accepts a number and returns the nth number
- * in the Fibonacci sequence
- */
-
-function fib(num) {
-  if (num < 3) return 1
-  return fib(num - 1) + fib(num - 2)
-}
