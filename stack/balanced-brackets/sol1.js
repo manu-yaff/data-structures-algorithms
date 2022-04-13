0a66c39bec2d6e185bcfad6c3272f8c745cc7378
manu-yaff <manuelyg2097@gmail.com>
2022-04-13 14:07:50 -0500

    Add balanced brackets problem

---
+++ stack/balanced-brackets/sol1.js
@@ -0,0 +1,28 @@
+// Time: O(n), Space O(n)
+function balancedBrackets(string) {
+	let stack  = [];
+	const openingBrackets = '([{';
+	const closingBrackets = ')]}';
+	const matchingBrackets = {
+		")" : "(",
+		"]" : "[",
+		"}" : "{",
+	}
+	
+	for (const char of string) {
+		if (openingBrackets.includes(char)) {
+			stack.push(char);
+		} else if (closingBrackets.includes(char)) {
+			if (stack.length === 0) {
+				return false;
+			}
+			if (stack[stack.length - 1] === matchingBrackets[char]) {
+				stack.pop();
+			} else {
+				return false;
+			}
+		}
+	}
+	return stack.length === 0;
+}
+
