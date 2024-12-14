2332f48a36608e8be5939ef35b068ba65e2819d7
manu-yaff <manuelyg2097@gmail.com>
2024-12-14 08:57:39 -0600

    problem: Add "Reverse Linked List" problem

--- src/linked-list/reverse-list/index.js
+++ src/linked-list/reverse-list/index.js
@@ -1,29 +1,30 @@
 /**
- * Given the head of a singly linked list, your task is to reverse the list and
- * return its head. The singly linked list has nodes, and each node contains an
- * integer and a pointer to the next node. The last node in the list points to
- * null, indicating the end of the list.
- * Time complexity is O(n)
+ * Reverse Linked List
+ *
+ * Given the head of a singly linked list, reverse the list, and return the reversed list.
+ *
+ * Input: head = [1,2,3,4,5]
+ * Output: [5,4,3,2,1]
+ *
  */
+
+// Time complexity O(n)
+// Space complexity O(1)
 function reverseList(head) {
-  // if list is empty or only contains one element, return the head
-  if (head === null || head.next === null) return head;
+  if (head === null || head.next === null) {
+    return head;
+  }
 
   let prev = null;
   let current = head;
 
-  // point current to prev & update prev and current
-  // use next as an aux so that current can keep moving
   while (current !== null) {
     let next = current.next;
-
     current.next = prev;
-
     prev = current;
     current = next;
   }
 
   return prev;
 }
-
 module.exports = reverseList;
