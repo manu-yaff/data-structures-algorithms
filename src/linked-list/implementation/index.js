46bff7e221beadf05f9eaae80e4b90255e1e7127
manu-yaff <manuelyg2097@gmail.com>
2024-12-15 11:44:19 -0600

    chore: update node.value for node.val, so that leetcode problems match implementation

--- src/linked-list/implementation/index.js
+++ src/linked-list/implementation/index.js
@@ -12,8 +12,8 @@
  */
 
 class Node {
-  constructor(value) {
-    this.value = value;
+  constructor(val) {
+    this.val = val;
     this.next = null;
   }
 }
@@ -25,14 +25,14 @@ class LinkedList {
   }
 
   /**
-   * assuming we add the value at the end of the list
+   * assuming we add the val at the end of the list
    *
    * adding a node has only 2 cases:
    * - check case where head = null, then new node is both head == tail
    * - append node onto the end, then update tail reference
    */
-  add(value) {
-    const newNode = new Node(value);
+  add(val) {
+    const newNode = new Node(val);
 
     if (this.head === null) {
       this.head = this.tail = newNode;
@@ -43,13 +43,13 @@ class LinkedList {
   }
 
   /**
-   * traverse the list comparing the given value with the current node
+   * traverse the list comparing the given val with the current node
    *
    */
-  contains(value) {
+  contains(val) {
     let currentNode = this.head;
 
-    while (currentNode !== null && currentNode.value !== value) {
+    while (currentNode !== null && currentNode.val !== val) {
       currentNode = currentNode.next;
     }
 
@@ -65,15 +65,15 @@ class LinkedList {
    * 3 - removing the head node
    * 4 - removing the tail node
    * 5 - node to be removed is between head and tail
-   * 6 - value does not exist on the list
+   * 6 - val does not exist on the list
    */
-  remove(value) {
+  remove(val) {
     // case 1
     if (this.head === null) return false;
 
     let currentNode = this.head;
 
-    if (currentNode.value === value) {
+    if (currentNode.val === val) {
       if (this.head === this.tail) {
         // case 2
         this.head = this.tail = null;
@@ -84,7 +84,7 @@ class LinkedList {
       return true;
     }
 
-    while (currentNode.next !== null && currentNode.next.value !== value) {
+    while (currentNode.next !== null && currentNode.next.val !== val) {
       currentNode = currentNode.next;
     }
 
@@ -100,7 +100,7 @@ class LinkedList {
       return true;
     }
 
-    // case 6, value was not found
+    // case 6, val was not found
     return false;
   }
 
