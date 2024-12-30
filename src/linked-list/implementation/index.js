d1f604799fa7be3a9c9b211ca5cdd5ec55875496
manu-yaff <manuelyg2097@gmail.com>
2024-12-30 11:49:46 -0600

    implementation: update "Linked list" implementation - add ADT according to reference book

--- src/linked-list/implementation/index.js
+++ src/linked-list/implementation/index.js
@@ -1,138 +1,309 @@
-/**
- * Linked list
- *
- * Why?
- * - dynamic size
- * - insertion at the front or back is O(1)
- *
- * Operations
- * - insertion O(1) -> insertion in the middle is O(n)
- * - deletion O(n)
- * - insertion O(n)
- * - traversal in reverse order O(n^2)
- */
-
 class Node {
-  constructor(val) {
-    this.val = val;
-    this.next = null;
+  #data;
+  #next;
+
+  constructor(data, next = null) {
+    this.#data = data;
+    this.#next = next;
+    Object.freeze(this);
+  }
+
+  setData(data) {
+    this.#data = data;
+  }
+
+  getData() {
+    return this.#data;
+  }
+
+  setNext(next) {
+    this.#next = next;
+  }
+
+  getNext() {
+    return this.#next;
   }
 }
 
+/**
+ * Class representing unordered linked list
+ */
 class LinkedList {
+  #head;
+
   constructor() {
-    this.head = null;
-    this.tail = null;
+    this.#head = null;
+
+    Object.freeze(this);
   }
 
   /**
-   * assuming we add the val at the end of the list
-   *
-   * adding a node has only 2 cases:
-   * - check case where head = null, then new node is both head == tail
-   * - append node onto the end, then update tail reference
+   * Adds item to the start of the list
+   * @param {any} item The item to be added
    */
-  add(val) {
-    const newNode = new Node(val);
+  add(item) {
+    const newNode = new Node(item);
 
-    if (this.head === null) {
-      this.head = this.tail = newNode;
+    newNode.setNext(this.getHead());
+
+    this.#head = newNode;
+  }
+
+  /**
+   * Adds item to the end of the list
+   * @param {any} item The item to be added
+   */
+  append(item) {
+    const newNode = new Node(item);
+
+    if (this.#head === null) {
+      return this.add(item);
     } else {
-      this.tail.next = newNode;
-      this.tail = newNode;
-    }
-  }
+      const lastPosition = this.getLength() - 1;
 
-  /**
-   * traverse the list comparing the given val with the current node
-   *
-   */
-  contains(val) {
-    let currentNode = this.head;
+      const prevOfLast = this.#findPrevOfPosition(lastPosition);
 
-    while (currentNode !== null && currentNode.val !== val) {
-      currentNode = currentNode.next;
-    }
-
-    if (currentNode === null) return false;
-
-    return true;
-  }
-
-  /**
-   * we need to account for the cases:
-   * 1 - list is empty
-   * 2 - node to be removed is the only node
-   * 3 - removing the head node
-   * 4 - removing the tail node
-   * 5 - node to be removed is between head and tail
-   * 6 - val does not exist on the list
-   */
-  remove(val) {
-    // case 1
-    if (this.head === null) return false;
-
-    let currentNode = this.head;
-
-    if (currentNode.val === val) {
-      if (this.head === this.tail) {
-        // case 2
-        this.head = this.tail = null;
+      // means there is no prev cause the list is only 1 element
+      if (prevOfLast === null) {
+        this.#head.setNext(newNode);
       } else {
-        // case 3
-        this.head = this.head.next;
+        const lastItem = prevOfLast.getNext();
+
+        lastItem.setNext(newNode);
       }
-      return true;
-    }
-
-    while (currentNode.next !== null && currentNode.next.val !== val) {
-      currentNode = currentNode.next;
-    }
-
-    if (currentNode.next !== null) {
-      if (currentNode.next === this.tail) {
-        // case 4
-        this.tail = currentNode;
-      }
-
-      // case 5
-      currentNode.next = currentNode.next.next;
-
-      return true;
-    }
-
-    // case 6, val was not found
-    return false;
-  }
-
-  /**
-   * helper to display list elements
-   * for testing purposes
-   */
-  static print(head) {
-    let currentNode = head;
-
-    while (currentNode !== null) {
-      console.log(currentNode.val);
-      currentNode = currentNode.next;
     }
   }
 
   /**
-   * helper to get list as an array of elements
-   * for testing purposes
+   * Inserts item at the given position
+   * @param {number} pos The position where the element will be inserted
+   * @param {any} item The item to be added
    */
-  static getAsArray(head) {
+  insertAt(pos, item) {
+    const listLength = this.getLength();
+    const newNode = new Node(item);
+
+    if (pos > listLength - 1) {
+      return;
+    } else {
+      if (listLength === 1) {
+        this.add(item);
+      } else {
+        const prevOfPosition = this.#findPrevOfPosition(pos);
+        const currentNode = prevOfPosition.getNext();
+
+        prevOfPosition.setNext(newNode);
+        newNode.setNext(currentNode);
+      }
+    }
+  }
+
+  /**
+   * Removes the item from the list. Assumes the item is in the list
+   * @param {any} item The item to be remove
+   * @returns {any} Returns the removed item
+   */
+  remove(item) {
+    if (this.#head !== null) {
+      // list has only one element
+      if (this.#head.getNext() === null) {
+        const temp = this.#head;
+
+        this.#head = null;
+
+        return temp.getData();
+      } else {
+        const prevOfItem = this.#findPrevOfItem(item);
+        const currentItem = prevOfItem.getNext();
+
+        prevOfItem.setNext(currentItem.getNext());
+
+        return currentItem.getData();
+      }
+    }
+  }
+
+  /**
+   * Removes the last element of the list
+   * @returns {(any | null)} The last element of the list or null if empty list
+   */
+  pop() {
+    if (this.#head === null) {
+      return null;
+    } else if (this.#head.getNext() === null) {
+      const temp = this.#head;
+
+      this.#head = null;
+
+      return temp.getData();
+    } else {
+      const lastPosition = this.getLength() - 1;
+
+      const prevOfItem = this.#findPrevOfPosition(lastPosition);
+      const lastItem = prevOfItem.getNext();
+
+      prevOfItem.setNext(lastItem.getNext());
+
+      return lastItem.getData();
+    }
+  }
+
+  /**
+   * Removes item at the given position
+   * @param {number} post The position of the item to be removed
+   * @returns {(any)} Item removed
+   */
+  popAt(pos) {
+    const length = this.getLength();
+    const lastIndex = length - 1;
+
+    if (pos > lastIndex && length < 1) {
+      return;
+    } else if (pos === 0) {
+      const temp = this.#head;
+
+      this.#head = this.#head.getNext();
+
+      return temp.getData();
+    } else {
+      const prev = this.#findPrevOfPosition(pos);
+      const itemToRemove = prev.getNext();
+
+      prev.setNext(itemToRemove.getNext());
+
+      return itemToRemove.getData();
+    }
+  }
+
+  /**
+   * Checks if given item is in the list
+   * @param {any} item The item to be found
+   * @returns {boolean} True if item is found in the list, false otherwise
+   */
+  contains(item) {
+    return Boolean(this.#findItem(item));
+  }
+
+  /**
+   * Checks if the list is empty
+   * @returns {boolean} True if the list is empty, otherwise false
+   */
+  isEmpty() {
+    return this.#head === null;
+  }
+
+  /**
+   * Retrieves the list length
+   * @returns {number} The number of items in the list
+   */
+  getLength() {
+    let counter = 0;
+    let current = this.#head;
+
+    while (current !== null) {
+      current = current.getNext();
+      counter++;
+    }
+
+    return counter;
+  }
+
+  /**
+   * Retrieves the index of the given item, starting at 0
+   * @param {any} item The item which will be used to find the index
+   * @returns {(number | null)} The item's index, or null if item is not in the list
+   */
+  getIndexOfItem(item) {
+    let count = 0;
+    let current = this.#head;
+
+    while (current !== null && current.getData() !== item) {
+      current = current.getNext();
+      count++;
+    }
+
+    if (current === null) {
+      return null;
+    } else {
+      return count;
+    }
+  }
+
+  /**
+   * Retrieves head
+   * @returns {(Node | null)} The head pointer
+   */
+  getHead() {
+    return this.#head;
+  }
+
+  /**
+   * Retrieves list items in an array. The first item in the array is head.
+   * Use for testing purposes
+   * @returns {Array} Array containing the list elements
+   */
+  getAsArray() {
     const array = [];
 
-    let currentNode = head;
+    let current = this.#head;
 
-    while (currentNode !== null) {
-      array.push(currentNode.val);
-      currentNode = currentNode.next;
+    while (current !== null) {
+      array.push(current.getData());
+      current = current.getNext();
     }
 
     return array;
+  }
+
+  /**
+   * Retrieves the given item's node
+   * @param {any} item The item to be found
+   * @returns {(Node | null)} The node found, or null if not found
+   */
+  #findItem(item) {
+    let current = this.#head;
+
+    while (current !== null && current.getData() !== item) {
+      current = current.getNext();
+    }
+
+    return current;
+  }
+
+  /**
+   * Finds the previous element of the given item
+   * @param {any} item Item who will be used to find prev
+   * @returns {(Node | null)} The previous node of item, or null
+   */
+  #findPrevOfItem(item) {
+    let current = this.#head;
+    let prev = null;
+
+    while (current !== null && current.getData() !== item) {
+      prev = current;
+      current = current.getNext();
+    }
+
+    return prev;
+  }
+
+  /**
+   * Finds the previous node of the given position
+   * @param {any} pos Position which will be used to find prev
+   * @returns {(Node | null)} The previous node of position, or null
+   */
+  #findPrevOfPosition(pos) {
+    let current = this.#head;
+    let prev = null;
+    let counter = 0;
+
+    while (current !== null && counter < pos) {
+      prev = current;
+      current = current.getNext();
+      counter++;
+    }
+
+    return prev;
   }
 }
 
--- src/linked-list/implementation/index.test.js
+++ src/linked-list/implementation/index.test.js
@@ -1,90 +1,401 @@
 const LinkedList = require('./index');
 
 describe(LinkedList.name, () => {
-  describe(LinkedList.prototype.add, () => {
-    it('should numbers to list', () => {
-      const list = new LinkedList();
+  it('should initialize linked list head', () => {
+    // Arrange
 
-      list.add(1);
-      list.add(2);
-      list.add(3);
-      list.add(4);
+    // Act
+    const l = new LinkedList();
+
+    // Assert
+    expect(l.getHead()).toBe(null);
+  });
+
+  describe(LinkedList.prototype.isEmpty.name, () => {
+    it('should return true for empty list', () => {
+      // Arrange
+      const l = new LinkedList();
+
+      // Act
+      const isEmpty = l.isEmpty();
+
+      // Assert
+      expect(isEmpty).toBe(true);
+    });
+
+    it('should return false for non empty list', () => {
+      // Arrange
+      const l = new LinkedList();
+
+      l.add(29);
+      l.add(0);
+      l.add(10);
+
+      // Act
+      const isEmpty = l.isEmpty();
+
+      // Assert
+      expect(isEmpty).toBe(false);
+      expect(l.getAsArray()).toEqual([10, 0, 29]);
     });
   });
 
-  describe(LinkedList.prototype.contains, () => {
-    const list = new LinkedList();
+  describe(LinkedList.prototype.getLength.name, () => {
+    it('should return 0 for list with 0 items', () => {
+      // Arrange
+      const l = new LinkedList();
 
-    list.add(1);
-    list.add(2);
-    list.add(3);
+      // Act
+      const length = l.getLength();
 
+      // Assert
+      expect(length).toBe(0);
+      expect(l.getAsArray()).toEqual([]);
+    });
+
+    it('should return 5 for list with 5 items', () => {
+      // Arrange
+      const l = new LinkedList();
+
+      l.add(1);
+      l.add(2);
+      l.add(3);
+      l.add(4);
+      l.add(5);
+
+      // Act
+      const length = l.getLength();
+
+      // Assert
+      expect(length).toBe(5);
+      expect(l.getAsArray()).toEqual([5, 4, 3, 2, 1]);
+    });
+  });
+
+  describe(LinkedList.prototype.add.name, () => {
+    it('should add element to the beginning when list is empty', () => {
+      // Arrange
+      const l = new LinkedList();
+
+      // Act
+      l.add(28);
+
+      // Assert
+      expect(l.getLength()).toBe(1);
+      expect(l.getAsArray()).toEqual([28]);
+    });
+
+    it('should add element at the beginning when the list is not empty', () => {
+      // Arrange
+      const l = new LinkedList();
+      l.add(28);
+      l.add(2);
+      l.add(8);
+
+      // Act
+      l.add(90);
+
+      // Assert
+      expect(l.getAsArray()).toEqual([90, 8, 2, 28]);
+      expect(l.getLength()).toBe(4);
+      expect(l.getHead().getData()).toBe(90);
+    });
+  });
+
+  describe(LinkedList.prototype.contains.name, () => {
     it('should return true when item is found in the list', () => {
-      expect(list.contains(1)).toBe(true);
+      // Arrange
+      const l = new LinkedList();
+      l.add(28);
+      l.add(2);
+      l.add(8);
+
+      // Act
+      const found = l.contains(28);
+
+      // Assert
+      expect(found).toBe(true);
     });
 
-    it('should return false when item is found in the list', () => {
-      expect(list.contains(400)).toBe(false);
+    it('should return false when item is not found in the list', () => {
+      // Arrange
+      const l = new LinkedList();
+      l.add(28);
+      l.add(2);
+      l.add(8);
+
+      // Act
+      const found = l.contains(1298);
+
+      // Assert
+      expect(found).toBe(false);
     });
   });
 
-  describe(LinkedList.prototype.remove, () => {
-    it('should return false when list is empty', () => {
-      const list = new LinkedList();
+  describe(LinkedList.prototype.getIndexOfItem.name, () => {
+    it.each([
+      {
+        item: 1,
+        index: 4,
+      },
+      {
+        item: 2,
+        index: 3,
+      },
+      {
+        item: 3,
+        index: 2,
+      },
+      {
+        item: 4,
+        index: 1,
+      },
+      {
+        item: 5,
+        index: 0,
+      },
+      {
+        item: 6,
+        index: null,
+      },
+    ])('should return index $index for item $item', ({ item, index }) => {
+      // Arrange
+      const l = new LinkedList();
 
-      expect(list.remove(10)).toBe(false);
-      expect(list.contains(10)).toBe(false);
+      l.add(1);
+      l.add(2);
+      l.add(3);
+      l.add(4);
+      l.add(5);
+
+      // Act
+      const result = l.getIndexOfItem(item);
+
+      // Assert
+      expect(result).toBe(index);
+    });
+  });
+
+  describe(LinkedList.prototype.append.name, () => {
+    it('should add element to the end of the list when it is empty', () => {
+      // Arrange
+      const l = new LinkedList();
+
+      // Act
+      l.append(58);
+
+      // Assert
+      expect(l.getLength()).toBe(1);
+      expect(l.getAsArray()).toEqual([58]);
     });
 
-    it('should return true when list contains only the element to be removed', () => {
-      const list = new LinkedList();
+    it('should add element to the end of the list when list has one item', () => {
+      // Arrange
+      const l = new LinkedList();
+      l.add(1);
 
-      list.add(100);
+      // Act
+      l.append(58);
 
-      expect(list.remove(100)).toBe(true);
-      expect(list.contains(100)).toBe(false);
+      // Assert
+      expect(l.getLength()).toBe(2);
+      expect(l.getAsArray()).toEqual([1, 58]);
     });
 
-    it('should return true when the element to be removed is head', () => {
-      const list = new LinkedList();
+    it('should add element to the end of the list when list has more than one item', () => {
+      // Arrange
+      const l = new LinkedList();
+      l.add(1);
+      l.add(2);
+      l.add(3);
+      l.add(4);
 
-      list.add(100);
-      list.add(200);
+      // Act
+      l.append(100);
 
-      expect(list.remove(100)).toBe(true);
-      expect(list.contains(100)).toBe(false);
+      // Assert
+      expect(l.getLength()).toBe(5);
+      expect(l.getAsArray()).toEqual([4, 3, 2, 1, 100]);
+    });
+  });
+
+  describe(LinkedList.prototype.insertAt.name, () => {
+    it('should not add element in a position greater than the list length', () => {
+      // Arrange
+      const l = new LinkedList();
+
+      // Act
+      l.insertAt(0, 10);
+
+      // Assert
+      expect(l.getAsArray()).toEqual([]);
     });
 
-    it('should return true when the element to be removed is tail', () => {
-      const list = new LinkedList();
+    it('should insert item at position 0', () => {
+      // Arrange
+      const l = new LinkedList();
+      l.add(20);
 
-      list.add(100);
-      list.add(200);
+      // Act
+      l.insertAt(0, 10);
 
-      expect(list.remove(200)).toBe(true);
-      expect(list.contains(200)).toBe(false);
+      // Assert
+      expect(l.getAsArray()).toEqual([10, 20]);
     });
 
-    it('should return true then the element is between head and tail', () => {
-      const list = new LinkedList();
+    it('should add item into a middle position', () => {
+      // Arrange
+      const l = new LinkedList();
+      l.add(20);
+      l.add(30);
+      l.add(50); // list = [50, 30, 20]
 
-      list.add(100);
-      list.add(300);
-      list.add(200);
+      // Act
+      l.insertAt(1, 19);
 
-      expect(list.remove(300)).toBe(true);
-      expect(list.contains(300)).toBe(false);
+      // Assert
+      expect(l.getAsArray()).toEqual([50, 19, 30, 20]);
     });
 
-    it('should return false when the element is not found in the list', () => {
-      const list = new LinkedList();
+    it('should add item into the last position', () => {
+      // Arrange
+      const l = new LinkedList();
+      l.add(20);
+      l.add(30);
+      l.add(50); // list = [50, 30, 20]
 
-      list.add(100);
-      list.add(300);
-      list.add(200);
+      // Act
+      l.insertAt(2, 40);
 
-      expect(list.remove(500)).toBe(false);
-      expect(list.contains(500)).toBe(false);
+      // Assert
+      expect(l.getAsArray()).toEqual([50, 30, 40, 20]);
+    });
+  });
+
+  describe(LinkedList.prototype.remove.name, () => {
+    it('should remove element that is found at position 0', () => {
+      // Arrange
+      const l = new LinkedList();
+      l.add(100);
+
+      // Arrange
+      const removedItem = l.remove(100);
+
+      // Arrange
+      expect(removedItem).toBe(100);
+      expect(l.getAsArray()).toEqual([]);
+    });
+
+    it('should remove element that is found at the last position', () => {
+      // Arrange
+      const l = new LinkedList();
+      l.add(100);
+      l.add(200);
+      l.add(300); // [300, 200, 100]
+
+      // Arrange
+      const removedItem = l.remove(100);
+
+      // Arrange
+      expect(removedItem).toBe(100);
+      expect(l.getAsArray()).toEqual([300, 200]);
+    });
+
+    it('should remove element that is found between first and last position', () => {
+      // Arrange
+      const l = new LinkedList();
+      l.add(100);
+      l.add(99);
+      l.add(-1);
+      l.add(200);
+      l.add(444);
+      l.add(300); // [300, 444, 200, -1, 99, 100]
+
+      // Arrange
+      const removedItem = l.remove(-1);
+
+      // Arrange
+      expect(removedItem).toBe(-1);
+      expect(l.getAsArray()).toEqual([300, 444, 200, 99, 100]);
+    });
+  });
+
+  describe(LinkedList.prototype.pop.name, () => {
+    it('should remove element from the end of the list when list has only one element', () => {
+      // Arrange
+      const l = new LinkedList();
+      l.add(100);
+
+      // Arrange
+      const removedItem = l.pop();
+
+      // Arrange
+      expect(removedItem).toBe(100);
+      expect(l.getAsArray()).toEqual([]);
+    });
+
+    it('should remove element from the end of the list when list has multiple elements', () => {
+      // Arrange
+      const l = new LinkedList();
+      l.add(100);
+      l.add(0);
+
+      // Arrange
+      const removedItem = l.pop();
+
+      // Arrange
+      expect(removedItem).toBe(100);
+      expect(l.getAsArray()).toEqual([0]);
+    });
+  });
+
+  describe(LinkedList.prototype.popAt.name, () => {
+    it('should pop item from position 0', () => {
+      // Arrange
+      const l = new LinkedList();
+      l.add(1);
+      l.add(2);
+      l.add(3); // [3, 2, 1]
+
+      // Arrange
+      const removedItem = l.popAt(0);
+
+      // Arrange
+      expect(removedItem).toBe(3);
+      expect(l.getAsArray()).toEqual([2, 1]);
+    });
+
+    it('should pop item from last position', () => {
+      // Arrange
+      const l = new LinkedList();
+      l.add(1);
+      l.add(2);
+      l.add(3);
+      l.add(4); // [4, 3, 2, 1]
+
+      // Arrange
+      const removedItem = l.popAt(3);
+
+      // Arrange
+      expect(removedItem).toBe(1);
+      expect(l.getAsArray()).toEqual([4, 3, 2]);
+    });
+
+    it('should pop item from intermediate position', () => {
+      // Arrange
+      const l = new LinkedList();
+      l.add(1);
+      l.add(2);
+      l.add(3);
+      l.add(4); // [4, 3, 2, 1]
+
+      // Arrange
+      const removedItem = l.popAt(2);
+
+      // Arrange
+      console.log(l.getAsArray());
+      expect(removedItem).toBe(2);
+      expect(l.getAsArray()).toEqual([4, 3, 1]);
     });
   });
 });
