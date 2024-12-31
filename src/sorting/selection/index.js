61f37f9f5a8e11dda7c4793386b1239c69a6d239
manu-yaff <manuelyg2097@gmail.com>
2024-12-30 18:08:46 -0600

    implementation: update selection sort implementation

--- src/sorting/selection/index.js
+++ src/sorting/selection/index.js
@@ -1,33 +1,24 @@
 /**
- * Selection sort
- *
- * Assuming items are sorted in increasing-order
- *
- * Idea:
- * - move the smallest element to the beginning
- * - keep track of the min, at the end of the second loop, we swap the smallest
- * value with the current position of the first loop
- *
+ * Sorts array in ascending order with selection sort algorithm. Modifies the array
+ * in place
+ * @param {array} list The list to be sorted
+ * @returns {array} The list sorted in ascending order
  */
-
-// Time complexity O(n^2)
-// Space complexity O(1)
-function selectionSort(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    let currentMinIndex = i;
-
-    for (let j = i + 1; j < arr.length; j++) {
-      if (arr[j] < arr[currentMinIndex]) {
-        currentMinIndex = j;
+function selectionSort(list) {
+  for (let i = 0; i < list.length; i++) {
+    let maxIndex = 0;
+    for (let j = 0; j < list.length - i; j++) {
+      if (list[j] > list[maxIndex]) {
+        maxIndex = j;
       }
     }
 
-    let temp = arr[i];
-    arr[i] = arr[currentMinIndex];
-    arr[currentMinIndex] = temp;
+    let temp = list[list.length - 1 - i];
+    list[list.length - 1 - i] = list[maxIndex];
+    list[maxIndex] = temp;
   }
 
-  return arr;
+  return list;
 }
 
 module.exports = selectionSort;
