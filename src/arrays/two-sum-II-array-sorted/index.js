27ecd36d37fc1e52cbc59b43bd27111e8d87b385
manu-yaff <manuelyg2097@gmail.com>
2025-01-05 20:59:42 -0600

    problem: add "Two sum II - Array sorted" problem"

---
+++ src/arrays/two-sum-II-array-sorted/index.js
@@ -0,0 +1,23 @@
+/**
+ * Two Sum II - Input Array Is Sorted
+ * Link: https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/
+ */
+
+function twoSum(nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left < right) {
+    const sum = nums[left] + nums[right];
+
+    if (sum === target) {
+      return [left + 1, right + 1];
+    } else if (sum > target) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+}
+
+module.exports = twoSum;
---
+++ src/arrays/two-sum-II-array-sorted/index.test.js
@@ -0,0 +1,26 @@
+const twoSum = require('./index');
+
+describe(twoSum.name, () => {
+  it.each([
+    {
+      nums: [2, 7, 11, 15],
+      target: 9,
+      expectedResult: [1, 2],
+    },
+    {
+      nums: [2, 3, 4],
+      target: 6,
+      expectedResult: [1, 3],
+    },
+    {
+      nums: [-1, 0],
+      target: -1,
+      expectedResult: [1, 2, 6],
+    },
+  ])(
+    'twoSum($nums, $target) => $expectedResult',
+    ({ nums, target, expectedResult }) => {
+      expect(twoSum(nums, target)).toEqual(expectedResult);
+    }
+  );
+});
