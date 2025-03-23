577464aeeb3f9c636c472974036bb7b7eb21df5b
manu-yaff <manuelyg2097@gmail.com>
2025-03-23 14:36:27 -0600

    problem: update "Length of Last Word" problem

--- src/strings/leetcode-style-problems/length-of-last-word/index.js
+++ src/strings/leetcode-style-problems/length-of-last-word/index.js
@@ -3,6 +3,17 @@
  * Link: https://leetcode.com/problems/length-of-last-word/description/
  */
 function lengthOfLastWord(s) {
-  const array = s.trim().split(' ');
-  return array[array.length - 1].length;
+  let count = 0;
+  let start = s.length - 1;
+
+  while (s[start] === ' ' && start >= 0) {
+    start--;
+  }
+
+  while (s[start] !== ' ' && start >= 0) {
+    count += 1;
+    start--;
+  }
+
+  return count;
 }
