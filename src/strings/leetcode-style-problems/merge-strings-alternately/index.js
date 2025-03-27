08fcc810a7c7fb1b1d2ec4ca328b761bfabe6692
manu-yaff <manuelyg2097@gmail.com>
2025-03-27 11:04:53 -0600

    problem: add "Merge Strings Alternately" problem

---
+++ src/strings/leetcode-style-problems/merge-strings-alternately/index.js
@@ -0,0 +1,29 @@
+/**
+ * Merge Strings Alternately
+ * Link: https://leetcode.com/problems/merge-strings-alternately/description/
+ */
+function mergeAlternately(word1, word2) {
+  let result = '';
+  let i = 0;
+  let j = 0;
+
+  while (i < word1.length && j < word2.length) {
+    result += word1[i];
+    result += word2[j];
+
+    i++;
+    j++;
+  }
+
+  while (i < word1.length) {
+    result += word1[i];
+    i++;
+  }
+
+  while (j < word2.length) {
+    result += word2[j];
+    j++;
+  }
+
+  return result;
+}
